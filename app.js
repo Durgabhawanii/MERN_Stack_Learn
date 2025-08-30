3f5e4d99e460decb5779f5a869b3301be318ce63
Durgabhawanii <durgabhawanii2020@gmail.com>
2025-08-30 16:38:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const a = 10;
+const b = 21;
+const myAge = "I am : " + (a+b) + " Years Old" ;
+const gret  = "My Name is : "
+const name = "Durga Bahadur Shrestha";
+// console.log(myAge);
+// console.log(gret + name);
+
+// console.log("Welcome to the MERN Stack Development Course");
+// Array
+const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers[1]);
+// console.log(numbers.length);
+
+
+// Object
+const person = {
+    firstName: "Durga",
+    middleName: "Bahadur",
+    lastName: "Shrestha",
+    age: 31
+};
+// {key:value}
+console.log("My Name is : " + person.firstName);
+console.log(person.middleName);
+console.log(person.lastName);
+console.log(person.age);
+// concationation 
+console.log("My Full Name is : " + person.firstName + " " + person.middleName + " " + person.lastName + " and I am " + person.age + " Years Old.");
